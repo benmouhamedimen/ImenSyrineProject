c5570cbd6cabf4c76bd1614f40d84d7a702f741f
imen ben mohamed <imen.benohamed@outlook.fr>
2017-02-04 10:43:31 +0100

    creation de classe utlisateur

---
+++ UserApp/UserApp/Model/Utilisateur.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserApp.Model
+{
+    class Utilisateur
+    {
+        public String Id { get; set; }
+        public String nom { get; set; }
+        public String prenom { get; set; }
+        public String adresse { get; set; }
+        public String motDePasse { get; set; }
+        public Int32 cin { get; set; }
+        public String dateNaissance { get; set; }
+        public Int32 telephone { get; set; }
+        public String sexe { get; set; }
+
+
+    }
+}
