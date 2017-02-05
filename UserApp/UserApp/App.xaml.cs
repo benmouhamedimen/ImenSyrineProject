6d8cd004a2da10c119b5930f7ab66d0def614579
syrinedaouas <sirine.ds123@gmail.com>
2017-02-05 01:55:31 -0800

    creation base de donnee

--- UserApp/UserApp/App.xaml.cs
+++ UserApp/UserApp/App.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using UserApp.View;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
@@ -22,6 +24,10 @@ namespace UserApp
     /// </summary>
     sealed partial class App : Application
     {
+        public static MobileServiceClient MobileService =
+new MobileServiceClient(
+    "https://cooksocial.azurewebsites.net"
+);
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
@@ -66,7 +72,7 @@ namespace UserApp
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(Connexion), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
--- UserApp/UserApp/View/Connexion.xaml.cs
+++ UserApp/UserApp/View/Connexion.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using UserApp.Model;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +25,48 @@ namespace UserApp.View
     /// </summary>
     public sealed partial class Connexion : Page
     {
+        private MobileServiceCollection<Utilisateur, Utilisateur> utilisateurs;
+        private MobileServiceCollection<Utilisateur, Utilisateur> utilisateurs1;
+        private IMobileServiceTable<Utilisateur> utilisateurtable = App.MobileService.GetTable<Utilisateur>();
         public Connexion()
         {
             this.InitializeComponent();
+        }
+
+        private void btninscrit_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Inscrption));
+        }
+
+        private async void btncnx_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            try
+            {
+                utilisateurs = await utilisateurtable.Where(Utilisateur => Utilisateur.mail==txtemail.Text).ToCollectionAsync();
+                if (utilisateurs.Count>0)
+                {
+                    utilisateurs1 = await utilisateurtable.Where(Utilisateur => Utilisateur.mail ==txtemail.Text && Utilisateur.motDePasse==motdepasse.Password).ToCollectionAsync();
+                    if (utilisateurs1.Count()>0)
+                    {
+                        Frame.Navigate(typeof(Accueil));
+                    }
+                    else
+                    {
+                        MessageDialog msg = new MessageDialog("mot de passe invalid");
+                        msg.ShowAsync();
+                    }
+                }
+                else
+                {
+                    MessageDialog msg = new MessageDialog("verifier adresse email");
+                    msg.ShowAsync();
+                }
+            }
+            catch (Exception)
+            {
+                MessageDialog msg = new MessageDialog("erreur serveur");
+                msg.ShowAsync();
+            }
         }
     }
 }
